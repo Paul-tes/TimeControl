9a95f204bbbdaeb5f52130a03b05224cc1432642
paul <86291500+Paul-tes@users.noreply.github.com>
2022-06-21 13:24:54 -0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace TimeControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbMinute = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbMin = new System.Windows.Forms.Label();
+            this.lbSec = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lb_fName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lb_LastName = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbMinute
+            // 
+            this.lbMinute.AutoSize = true;
+            this.lbMinute.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMinute.Location = new System.Drawing.Point(52, 56);
+            this.lbMinute.Name = "lbMinute";
+            this.lbMinute.Size = new System.Drawing.Size(78, 26);
+            this.lbMinute.TabIndex = 0;
+            this.lbMinute.Text = "Minute";
+            this.lbMinute.Click += new System.EventHandler(this.lbMinute_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(168, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Second";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lbMin
+            // 
+            this.lbMin.AutoSize = true;
+            this.lbMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMin.Location = new System.Drawing.Point(57, 91);
+            this.lbMin.Name = "lbMin";
+            this.lbMin.Size = new System.Drawing.Size(69, 73);
+            this.lbMin.TabIndex = 3;
+            this.lbMin.Text = "0";
+            // 
+            // lbSec
+            // 
+            this.lbSec.AutoSize = true;
+            this.lbSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSec.Location = new System.Drawing.Point(178, 91);
+            this.lbSec.Name = "lbSec";
+            this.lbSec.Size = new System.Drawing.Size(69, 73);
+            this.lbSec.TabIndex = 4;
+            this.lbSec.Text = "0";
+            this.lbSec.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(35, 199);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(95, 28);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.start_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(153, 199);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(95, 28);
+            this.btnStop.TabIndex = 6;
+            this.btnStop.Text = "stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lb_fName
+            // 
+            this.lb_fName.AutoSize = true;
+            this.lb_fName.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_fName.Location = new System.Drawing.Point(12, 297);
+            this.lb_fName.Name = "lb_fName";
+            this.lb_fName.Size = new System.Drawing.Size(86, 21);
+            this.lb_fName.TabIndex = 7;
+            this.lb_fName.Text = "First Name";
+            this.lb_fName.Click += new System.EventHandler(this.lb_fName_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 297);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // lb_LastName
+            // 
+            this.lb_LastName.AutoSize = true;
+            this.lb_LastName.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_LastName.Location = new System.Drawing.Point(13, 345);
+            this.lb_LastName.Name = "lb_LastName";
+            this.lb_LastName.Size = new System.Drawing.Size(79, 19);
+            this.lb_LastName.TabIndex = 9;
+            this.lb_LastName.Text = "Last Name";
+            this.lb_LastName.Click += new System.EventHandler(this.lb_LastName_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(104, 344);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(199, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lb_LastName);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lb_fName);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lbSec);
+            this.Controls.Add(this.lbMin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbMinute);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbMinute;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbMin;
+        private System.Windows.Forms.Label lbSec;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lb_fName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lb_LastName;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbMinute_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            lbMin.Text = "0";
+            lbSec.Text = "0";
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            //btnStart.PerformClick();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            int sec = int.Parse(lbSec.Text);
+            int min = int.Parse((lbMin.Text));
+            if (sec < 60)
+            {
+                sec++;
+                lbSec.Text = sec.ToString();
+            }else
+            {
+                lbSec.Text = "0";
+                if (min < 60)
+                {
+                    min++;
+                    lbMin.Text = min.ToString();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lb_fName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lb_LastName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+ 
+        }
+    }
+}
